d14b9768d15c3ed4701c14bf7d535f6b38fce1f5
s1015256 <paul.koch@student.ru.nl>
2019-05-13 17:07:06 +0200

    ready for first test

---
+++ create_model.py
@@ -0,0 +1,86 @@
+from keras import Model
+from keras.layers import *
+from keras.applications import vgg16, resnet50
+import keras.backend as K
+from keras.utils import get_file
+from keras.layers.core import SpatialDropout2D, Activation
+from keras.layers import Flatten, Dropout
+from keras import layers
+
+
+def createResNet50(in_t, printmodel = False):
+    model = resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=in_t)
+
+    # freeze the layers
+    for layer in model.layers:
+        layer.trainable = False
+
+    # get the output of the model
+    x = model.layers[-2].output
+
+    x = Flatten()(x)
+
+    x = layers.Dense(256, activation='relu')(x)
+
+    x = layers.BatchNormalization()(x)
+
+    x = Dropout(0.5)(x)
+
+    x_out = layers.Dense(2, activation='softmax')(x)
+
+    ResNet_new = Model(input=in_t, output=x_out)
+
+    if(printmodel):
+        ResNet_new.summary()
+
+    return ResNet_new
+
+
+def CreateKaggleModel(printmodel=False):
+    kernel_size = (3, 3)
+    pool_size = (2, 2)
+    first_filters = 32
+    second_filters = 64
+    third_filters = 128
+
+    dropout_conv = 0.3
+    dropout_dense = 0.5
+
+    model = Sequential()
+    model.add(Conv2D(first_filters, kernel_size, activation='relu', input_shape=(96, 96, 3)))
+    model.add(Conv2D(first_filters, kernel_size, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=pool_size))
+    model.add(Dropout(dropout_conv))
+
+    model.add(Conv2D(second_filters, kernel_size, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Conv2D(second_filters, kernel_size, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=pool_size))
+    model.add(Dropout(dropout_conv))
+
+    model.add(Conv2D(third_filters, kernel_size, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Conv2D(third_filters, kernel_size, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPool2D(pool_size=pool_size))
+    model.add(Dropout(dropout_conv))
+
+    # model.add(GlobalAveragePooling2D())
+    model.add(Flatten())
+    model.add(Dense(256, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(Dropout(dropout_dense))
+    model.add(Dense(2, activation="softmax"))
+
+    if printmodel:
+        print(model.summary())
+
+    return model
---
+++ resnet-master/README.md
@@ -0,0 +1,73 @@
+# ResNet
+
+
+Overview
+--------
+ResNet serves as an extension to [Keras Applications](https://keras.io/applications/) to include
+- ResNet-101
+- ResNet-152
+
+The module is based on [Felix Yu](https://github.com/flyyufelix)'s implementation of ResNet-101 and ResNet-152, and his trained weights. Slight modifications have been made to make ResNet-101 and ResNet-152 have consistent API as those pre-trained models in
+[Keras Applications](https://keras.io/applications/). Code is also updated to Keras 2.0.
+
+
+Installation
+------------
+
+```shell
+pip install resnet
+```
+
+
+Usuage
+------
+
+```python
+import resnet
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+
+#-------------------------------------
+#   Load pre-trained models
+#-------------------------------------
+resnet50  = resnet.ResNet50(weights='imagenet')
+resnet101 = resnet.ResNet101(weights='imagenet')
+resnet152 = resnet.ResNet152(weights='imagenet')
+
+#-------------------------------------
+#   Helper functions
+#-------------------------------------
+def path_to_tensor(image_path, target_size):
+    image = load_img(image_path, target_size=target_size)
+    tensor = img_to_array(image)
+    tensor = np.expand_dims(tensor, axis=0)
+    return tensor
+
+#-------------------------------------
+#   Make predictions
+#-------------------------------------
+image_path = 'examples/images/dog.jpeg'
+image_tensor = path_to_tensor(image_path, (224, 224))
+pred_resnet50  = np.argmax(resnet50.predict(image_tensor))
+pred_resnet101 = np.argmax(resnet101.predict(image_tensor))
+pred_resnet152 = np.argmax(resnet152.predict(image_tensor))
+```
+
+
+![Sample dog image](examples/images/dog.jpeg)
+
+The above dog image is predicted to have
+-  257: 'Great Pyrenees' by ResNet-50
+-  257: 'Great Pyrenees' by ResNet-101
+-  257: 'Great Pyrenees' by ResNet-152
+
+
+Contact
+-------
+If you have any questions or encounter any bugs, please contact the author (Feiyang Niu, statech.forums@gmail.com)
+
+
+References
+----------
+- He and etc 2015 Deep Residual Learning for Image Recognition [arXiv:1512.03385](https://arxiv.org/abs/1512.03385)
+- Felix Yu's original implementation of ResNet-101 is found [here](https://gist.github.com/flyyufelix/65018873f8cb2bbe95f429c474aa1294) and ResNet-152 [here](https://gist.github.com/flyyufelix/7e2eafb149f72f4d38dd661882c554a6).
